6c2b6c246bfc5c457867c469b2b7c8dc6fff7c95
AJavier55 <aliciajavier5@gmail.com>
2020-11-07 16:02:39 -0500

    some login

--- playlist-frontend/src/App.js
+++ playlist-frontend/src/App.js
@@ -1,16 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react' 
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
+import TracklistContainer from './Containers/TracklistContainer'
+import Login from './Components/Login'
+import Signup from'./Components/Signup'
+import LoginCOntainer from './Containers/LoginContainer'
 
 class App extends React.Component {
   state = {
-    currentUser:{}
+    user: null
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem("token")
+    if (token) {
+      console.log('got token')
+      const options = {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+      fetch("http://localhost3000/api/v1/profile", options)
+      .then(res => res.json())
+      .then(data => this.setState({user: data.user}))
+    } else {
+      console.log('no token')
+      this.props.history.push("/login")
+    }
   }
 
   render() {
     return (
-      <div>
-        <h1>Playlist APP</h1>
+      <div className="app">
+        <Switch>
+          <Route path="/login" render={() => (<Login />)} />
+        </Switch>
       </div>
     )
   }
